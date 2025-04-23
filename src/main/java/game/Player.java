23438868abcfaf78e0a02bbccde43c3effa11dd5
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-04-22 20:39:18 -0400

    Remove test code

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -34,13 +34,6 @@ public class Player {
         unplayedDevCards = new CountCollection<DevCardType>();
         playedDevCards = new CountCollection<DevCardType>();
         victoryPoints = 0;
-
-
-        availableResources.add(Resource.ORE, 100);
-        availableResources.add(Resource.WHEAT, 100);
-        availableResources.add(Resource.SHEEP, 100);
-        availableResources.add(Resource.WOOD, 100);
-        availableResources.add(Resource.BRICK, 100);
     }
 
     public int getLongestRoad(){

672229453e90cbf7ff90b5a31709d10270654b2f
rhit-flannacj <119346129+rhit-flannacj@users.noreply.github.com>
2025-05-02 14:35:55 -0400

    SidePanelControllerTests generateController

---
+++ src/test/java/org/openjfx/ui/SidePanelControllerTests.java
@@ -0,0 +1,15 @@
+package org.openjfx.ui;
+
+import org.junit.jupiter.api.Test;
+
+public class SidePanelControllerTests {
+
+    public SidePanelController generateController() {
+        return new SidePanelController(null, null, null, null, null);
+    }
+
+    @Test
+    public void makeDiceTest() {
+
+    }
+}
